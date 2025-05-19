ff0a5a4fe9287b27f96a32feeb7ad7234993b831
hgy2124 <hgy2124@naver.com>
2025-05-19 13:18:21 +0900

    fix: 파일 업로드

---
+++ SK_Agentry_FE/docker-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NAME=sk059
+IMAGE_NAME="sk-agentry-fe"
+VERSION="2.0.0"
+
+# Docker 이미지 빌드
+docker build \
+  --tag ${NAME}-${IMAGE_NAME}:${VERSION} \
+  --file Dockerfile \
+  --platform linux/amd64 \
+  ${IS_CACHE} .
